50531bbc04f4803914f060e8375b7d534c57a5d0
KrishKoria <krishkoria2004@gmail.com>
2023-12-26 23:36:12 +0530

    debugging file upload

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
 });
 app.use((error, req, res, next) => {
     if(req.file){
+        console.log(req.file.path)
         fs.unlink(req.file.path, err => {
             console.log(err);
         });
@@ -34,7 +35,7 @@ app.use((error, req, res, next) => {
     if (res.headerSent) {
         return next(error);
     }
-    // res.status(error.code || 500).json({message: error.message || "An unknown error occurred!"});
+    res.status(error.code || 500).json({message: error.message || "An unknown error occurred!"});
 });
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sirovsp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(() => {
